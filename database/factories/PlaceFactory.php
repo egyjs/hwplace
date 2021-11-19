20c32b19280d23180a57f78d0ccea738293b5e5e
egyjs <el3zahaby@gmail.com>
2021-11-20 01:59:18 +0200

    seeder

--- database/factories/PlaceFactory.php
+++ database/factories/PlaceFactory.php
@@ -31,10 +31,10 @@ class PlaceFactory extends Factory
 
             'city_id'=> City::inRandomOrder()->first()->id,
             'section_id'=> Section::inRandomOrder()->first()->id,
-            'images' => $this->randomImage(),
-            'image' => $images[0],
+            'images' => $images,
+            'image' => 'https://picsum.photos/600/300.webp?'.rand(1, 105),
             'website'=> $this->faker->url(),
-            'google_map_location'=> '<div style="width: 100%"><iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q='.urlencode($name).'&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">vehicle gps tracker</a></iframe></div>',
+            'google_map_location'=> '<div style="width: 100%"><iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q='.urlencode($this->faker->city()).'&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">vehicle gps tracker</a></iframe></div>',
             'view_rates' => rand(0,1),
             'rates' => rand(0,5),
             'is_top' => rand(0,1),
